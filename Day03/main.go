6861cb6af5b7aadc6ab2ac86549093169ed6b86b
Francis Begyn <francis.begyn@gmail.com>
2017-12-03 17:44:39 +0100

    Worked Day 3 part 2 out the long way because I got bored

--- Day03/main.go
+++ Day03/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
 
 func main() {
-	prob1()
-	prob2()
+	prob1(368078.0)
+	prob2(368078)
 }
 
-func prob1() {
-	input := 13.0
+func prob1(input float64) {
 	var side float64
 
 	base := math.Ceil(math.Sqrt(input))
@@ -28,6 +28,106 @@ func prob1() {
 	fmt.Printf("The input has a Manhattan distance of %d \n", int(distance))
 }
 
-func prob2() {
+func prob2(input uint64) {
 	fmt.Printf("Too lazy, just looked the sequence up on OIES: http://oeis.org/A141481 \n")
+
+	spiral := [11][11]uint64{}
+	mask := make([][3]uint64, 3)
+	i := 11 / 2
+	j := 11 / 2
+
+	spiral[i][j] = 1
+
+	lLimit := 1
+	uLimit := 1
+	rLimit := 1
+	dLimit := 1
+	direction := 0
+	larger := false
+
+	for lLimit < 11 || uLimit < 11 || rLimit < 11 || dLimit < 11 || !larger {
+		if direction == 0 {
+			for l := 0; l < lLimit; l++ {
+				j++
+				mask = *applyMask(&spiral, i, j)
+				sum := calcSum(&mask)
+				larger = checkSum(input, sum)
+				spiral[i][j] = sum
+			}
+			lLimit++
+			rLimit++
+			direction = 1
+		} else if direction == 1 {
+			for u := 0; u < uLimit; u++ {
+				i--
+				mask = *applyMask(&spiral, i, j)
+				sum := calcSum(&mask)
+				larger = checkSum(input, sum)
+				spiral[i][j] = sum
+			}
+			uLimit++
+			dLimit++
+			direction = 2
+		} else if direction == 2 {
+			for r := 0; r < rLimit; r++ {
+				j--
+				mask = *applyMask(&spiral, i, j)
+				sum := calcSum(&mask)
+				larger = checkSum(input, sum)
+				spiral[i][j] = sum
+			}
+			rLimit++
+			lLimit++
+			direction = 3
+		} else if direction == 3 {
+			for d := 0; d < dLimit; d++ {
+				i++
+				mask = *applyMask(&spiral, i, j)
+				sum := calcSum(&mask)
+				larger = checkSum(input, sum)
+				spiral[i][j] = sum
+			}
+			dLimit++
+			uLimit++
+			direction = 0
+		}
+	}
+}
+
+func applyMask(spiral *[11][11]uint64, i int, j int) *[][3]uint64 {
+	mask := make([][3]uint64, 3)
+	for true {
+		if i-1 < 0 || i+1 > 10 || j-1 < 0 || j+1 > 10 {
+			break
+		}
+		x := 0
+		for ind, el := range *spiral {
+			if ind < i-1 || ind > i+1 {
+				continue
+			}
+			copy(mask[x][:3], el[j-1:j+2])
+			x++
+		}
+		break
+	}
+	return &mask
+}
+
+func calcSum(mask *[][3]uint64) uint64 {
+	var sum uint64
+	for _, el := range *mask {
+		for _, el2 := range el {
+			sum += el2
+		}
+	}
+	return sum
+}
+
+func checkSum(input uint64, sum uint64) bool {
+	if sum > input {
+		fmt.Printf("Got bored and did it racefully. The first number that is larger then the input is %d\n", sum)
+		os.Exit(0)
+		return true
+	}
+	return false
 }

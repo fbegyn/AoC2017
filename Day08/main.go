0ac5937efe6ea8d820e9c5d934eabc98836740df
Francis Begyn <francis.begyn@gmail.com>
2017-12-08 11:04:37 +0100

    Solved day 08

--- Day08/main.go
+++ Day08/main.go
@@ -1,13 +1,112 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	prob1()
-	prob2()
-}
-func prob1() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-}
+	scanner := bufio.NewScanner(f)
+	bank := make(map[string]int)
+	var highest int
 
-func prob2() {
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.Replace(line, ",", "", -1)
+		var phrase []string
+		linescan := bufio.NewScanner(strings.NewReader(line))
+		linescan.Split(bufio.ScanWords)
 
+		for linescan.Scan() {
+			word := linescan.Text()
+			phrase = append(phrase, word)
+		}
+
+		reg := phrase[0]
+		op := phrase[1]
+		amount, err := strconv.Atoi(phrase[2])
+		if err != nil {
+			panic(err)
+		}
+		a := phrase[4]
+		comp := phrase[5]
+		b, err := strconv.Atoi(phrase[6])
+		if err != nil {
+			panic(err)
+		}
+
+		switch comp {
+		case "<":
+			if bank[a] < b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		case ">":
+			if bank[a] > b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		case "<=":
+			if bank[a] <= b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		case ">=":
+			if bank[a] >= b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		case "==":
+			if bank[a] == b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		case "!=":
+			if bank[a] != b {
+				if op == "inc" {
+					bank[reg] += amount
+				} else if op == "dec" {
+					bank[reg] -= amount
+				}
+			}
+		}
+		for _, v := range bank {
+			if highest < v {
+				highest = v
+			}
+		}
+	}
+
+	var largest int
+	for _, v := range bank {
+		if largest < v {
+			largest = v
+		}
+	}
+	fmt.Printf("The largest register is %d.\n", largest)
+	fmt.Printf("The largest register during process is %d.\n", highest)
 }

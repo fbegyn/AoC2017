a0b891c009a0dd70e7e5a19629d315a8e4fe290a
Francis Begyn <francis.begyn@gmail.com>
2017-12-09 11:05:34 +0100

    Solved day 09 + better tests

--- Day09/main.go
+++ Day09/main.go
@@ -1,13 +1,57 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	prob1()
-	prob2()
-}
-func prob1() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-}
+	var value, score, countGarbage uint
+	garbage := false
+	scanner := bufio.NewScanner(f)
 
-func prob2() {
-
+	for scanner.Scan() {
+		line := scanner.Text()
+		//line := "<!!!>>"
+		for i := 0; i < len(line); i++ {
+			ch := line[i]
+			switch ch {
+			case '{':
+				if !garbage {
+					value++
+				} else if garbage {
+					countGarbage++
+				}
+			case '}':
+				if !garbage {
+					score += value
+					value--
+				} else if garbage {
+					countGarbage++
+				}
+			case '<':
+				if garbage {
+					countGarbage++
+				}
+				garbage = true
+			case '>':
+				garbage = false
+			case '!':
+				i++
+			default:
+				if garbage {
+					countGarbage++
+				}
+			}
+		}
+	}
+	fmt.Printf("The total score is %d\n", score)
+	fmt.Printf("The total amount of garbage characters is %d\n", countGarbage)
 }
--- Day09/main_test.go
+++ Day09/main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func BenchmarkDay09(b *testing.B) {
+	os.Stdout, _ = os.Open(os.DevNull)
 	for n := 0; n < b.N; n++ {
 		main()
 	}

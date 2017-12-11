124326dd5e6140c7245ca7183255f10f8635341c
Francis Begyn <francis.begyn@gmail.com>
2017-12-11 13:09:07 +0100

    Solved day 10

--- Day11/main.go
+++ Day11/main.go
@@ -1,13 +1,46 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
-	prob1()
-	prob2()
-}
-func prob1() {
-
-}
-
-func prob2() {
-
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	var line string
+	if scanner.Scan() {
+		line = scanner.Text()
+	}
+	steps := strings.Split(line, ",")
+	tracker := make(map[string]int)
+	var distance, maxDist int
+	for _, step := range steps {
+		tracker[step] += 1
+		vertical := tracker["n"] - tracker["s"]
+		diagonalLeft := tracker["nw"] - tracker["se"]
+		diagonalRight := tracker["ne"] - tracker["sw"]
+		a, b := vertical+diagonalRight, diagonalLeft+diagonalRight
+		if a < 0 {
+			a *= -1
+		}
+		if b < 0 {
+			b *= -1
+		}
+		if a > b {
+			distance = a
+		} else {
+			distance = b
+		}
+		if maxDist < distance {
+			maxDist = distance
+		}
+	}
+	fmt.Printf("He is %d tiles away and was at a max distance of %d\n", distance, maxDist)
 }
